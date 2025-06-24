c5e7bf22ae1c75b556a30d5d30953ac474b1a160
Balázs Sárdi <balazs.sardi@gmail.com>
2025-06-24 13:22:58 +0200

    added glassy effect on main page buttons

--- src/app/components/MainButton.tsx
+++ src/app/components/MainButton.tsx
@@ -43,29 +43,22 @@ export default function MainButton({
       transition={{ delay: delay, duration: 0.2 }}
     >
       <div
-        className="p-0.5 rounded-[20px]"
+        className="w-full h-full rounded-[20px] flex justify-evenly items-center flex-col text-center text-black p-4 border border-[#ECECEC] backdrop-blur-2xl"
         style={{
-          background: "linear-gradient(180deg, #F1F1F1 0%, #ECECEC 100%)",
+          background:
+            "linear-gradient(180deg, rgba(252, 252, 252, 0.85) 0%, rgba(244, 244, 244, 0.85) 100%)",
         }}
       >
-        <div
-          className="bg-white w-full h-full rounded-[18px] flex justify-evenly items-center flex-col text-center text-black p-4"
-          style={{
-            background:
-              "linear-gradient(180deg, rgba(252, 252, 252, 0.95) 0%, rgba(244, 244, 244, 0.95) 100%)",
-          }}
-        >
-          <h1 className={`leading-none text-[30px] ${libre.className}`}>
-            {title}
-          </h1>
-          <p className="leading-tight">{desc}</p>
-          <Image
-            src={`/${image}`}
-            alt={`${title} photo`}
-            width={235}
-            height={235}
-          />
-        </div>
+        <h1 className={`leading-none text-[30px] ${libre.className}`}>
+          {title}
+        </h1>
+        <p className="leading-tight">{desc}</p>
+        <Image
+          src={`/${image}`}
+          alt={`${title} photo`}
+          width={235}
+          height={235}
+        />
       </div>
     </motion.div>
   );
